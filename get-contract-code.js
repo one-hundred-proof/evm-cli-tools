971a10e268908a023987689e4c761014776975c5
100proof <security@100proof.org>
2025-01-28 10:59:00 +1000

    Add get-contract-code.js utility and edit diff-contract-code.js

--- get-contract-code.js
+++ get-contract-code.js
@@ -19,18 +19,11 @@ if (ETHERSCAN_API_DOMAIN) {
   etherscanApiDomain = ETHERSCAN_API_DOMAIN;
 }
 
-if (process.argv.length < 4) {
-  console.log(`Usage: ${path.basename(process.argv[1])} <address1> <address2> [<isWordLevelDiff = true> ]`);
+if (process.argv.length < 3) {
+  console.log(`Usage: ${path.basename(process.argv[1])} <address>`);
   exit(1);
 }
-const address1 = process.argv[2];
-const address2 = process.argv[3];
-
-let wordLevelDiff = true;
-const arg3 = process.argv[4]
-if (process.argv.length >= 5 && arg3[0] == 'f') {
-  wordLevelDiff = false;
-}
+const address = process.argv[2];
 
 const mkSourceCodeUrl = (address) => {
   return `https:///${etherscanApiDomain}/api?module=contract&action=getsourcecode&address=${address}&apikey=${ETHERSCAN_API_KEY}`
@@ -39,7 +32,6 @@ const mkSourceCodeUrl = (address) => {
 const mkDirAndWriteFile = (dir, fileName, content) => {
   fs.mkdirSync(path.join(dir, path.dirname(fileName)), { recursive: true });
   fs.writeFileSync(path.join(dir, fileName), content);
-
 }
 
 // Returns directory and flag whether it is multiple files or not
@@ -54,7 +46,7 @@ const getSourceFilesFromAddress = async (address) => {
 
   const sourceCode = data.result[0].SourceCode;
 
-  let dir = fs.mkdtempSync("temp-");
+  let dir = fs.mkdtempSync(`${address}-code-`);
   let areMultipleFiles = false;
 
   let sourceObj;
@@ -67,8 +59,6 @@ const getSourceFilesFromAddress = async (address) => {
     }
   }
 
-
-
   if (areMultipleFiles) {
     for (let fileName in sourceObj) {
       if (sourceObj.hasOwnProperty(fileName)) {
@@ -79,13 +69,6 @@ const getSourceFilesFromAddress = async (address) => {
     mkDirAndWriteFile(dir, "Source.sol", sourceCode);
   }
   return { dir: dir, areMultipleFiles: areMultipleFiles };
-}
-
-const r1 = await getSourceFilesFromAddress(address1);
-const r2 = await getSourceFilesFromAddress(address2);
-
-const rmRf = (dir) => {
-  fs.rmSync(dir, { recursive: true, force: true});
 }
 
 
@@ -105,62 +88,9 @@ const getFilesRecursively = (dir) => {
   return files;
 };
 
-const files1 = getFilesRecursively(r1.dir);
-const files2 = getFilesRecursively(r2.dir);
 
-console.log(`Address 1: ${address1}`);
-console.log(`Address 2: ${address2}`);
+const r = await getSourceFilesFromAddress(address);
+getFilesRecursively(r.dir);
+console.log(`Files saved in ${r.dir}`);
 
-if (r1.areMultipleFiles && r2.areMultipleFiles) {
-  console.log(`\n=== Files at first address but not second ===`);
-
-  for (let i in files1) {
-    let f = files1[i];
-    if (!files2.includes(f)) {
-      console.log(`  - ${f}`);
-    }
-  }
-
-  console.log(`\n=== Files at second address but not first ===`);
-  for (let i in files2) {
-    let f = files2[i];
-    if (!files1.includes(f)) {
-      console.log(`  - ${f}`);
-    }
-  }
-}
-
-if (r1.areMultipleFiles == r2.areMultipleFiles) {
-  console.log("\n=== Diffs follow ===")
-
-  const diffFiles = (f1, f2) => {
-    let args = ['diff', '--no-index', '--color'];
-    if (wordLevelDiff) {
-      args = args.concat(['--word-diff=plain']);
-    }
-    args = args.concat([f1, f2]);
-
-    const result = spawnSync('git', args);
-    if (result.status != 0) {
-      console.log(result.stdout.toString());
-    }
-
-  }
-
-  for (let i in files1) {
-    let f = files1[i];
-    if (files2.includes(f)) {
-      diffFiles(`${r1.dir}/${f}`, `${r2.dir}/${f}`);
-    }
-  }
-} else if (r1.areMultipleFiles) {
-  console.log(`Error: Multiple files at address ${address1} and not ${address2}`);
-} else if (r2.areMultipleFiles) {
-  console.log(`Error: Multiple files at address ${address2} and not ${address1}`);
-
-}
-
-// Clean up
-rmRf(r1.dir);
-rmRf(r2.dir);
 
