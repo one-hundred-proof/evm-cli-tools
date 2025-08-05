e982a490063db1982b4c171503c7fca570327d44
100proof <security@100proof.org>
2025-08-05 11:34:49 +1000

    Fix scripts

--- bin/evm-to-checksum-address.js
+++ bin/evm-to-checksum-address.js
@@ -6,35 +6,28 @@ import web3 from "web3";
 import chalk from 'chalk';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { setupYargs } from '../lib/config-utils.js';
 
-const argv = yargs(hideBin(process.argv))
-  .usage(`${chalk.bold('Usage:')} $0 [options] <address>...`)
-  .positional('address', {
-    describe: chalk.cyan('Ethereum address(es) to convert to checksum format'),
-    type: 'string',
-    demandOption: true
-  })
-  .example('$0 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', `${chalk.green('Convert address to checksum format')}`)
-  .example('$0 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 0x70997970c51812dc3a010c7d01b50e0d17dc79c8', `${chalk.green('Convert multiple addresses')}`)
-  .help()
-  .alias('help', 'h')
-  .parserConfiguration({
-    'description-colors': true
-  })
-  .updateStrings({
-    'Options:': chalk.yellow.bold('Options:'),
-    'Examples:': chalk.green.bold('Examples:'),
-    'Positionals:': chalk.magenta.bold('Positionals:')
-  })
-  .argv;
+const yargsInstance = setupYargs(yargs(hideBin(process.argv)))
+  .command('$0 <addresses...>', "", (yargs) => {
+    yargs.positional('addresses', {
+      describe: chalk.cyan('Ethereum address(es) to convert to checksum format'),
+      type: 'string',
+      demandOption: true
+    })
+    .example('$0 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', `${chalk.green('Convert address to checksum format')}`)
+    .example('$0 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 0x70997970c51812dc3a010c7d01b50e0d17dc79c8', `${chalk.green('Convert multiple addresses')}`)
+  });
 
-if (argv._.length === 0) {
+const argv = yargsInstance.parse();
+
+if (argv.length === 0) {
   console.error(chalk.red('Missing required address argument'));
   yargs.showHelp();
   process.exit(1);
 }
 
-for (const address of argv._) {
+for (const address of argv.addresses) {
   try {
     console.log(web3.utils.toChecksumAddress(address));
   } catch (error) {
