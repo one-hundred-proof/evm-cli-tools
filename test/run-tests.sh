292d60e0d787a0589eb2a5f6d8e7b6f5b4d730b5
100proof <security@100proof.org>
2025-08-05 13:44:28 +1000

    Add basic tests

---
+++ test/run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Really dumb tests that just "smoke test" the scripts
+#
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $THIS_DIR/../bin
+
+echo -e "\n[+] evm-pretty"
+./evm-pretty.js 1000000000000000000
+
+echo -e "\n[+] evm-to-checksum-address"
+./evm-to-checksum-address.js 0xbe6932de5de6e7f8fae2d9f013321aec36997a30 0xd5055a782813da3862e9c9980bb047511960749f
+
+echo -e "\n[+] diff-contract-code"
+./diff-contract-code.js --chain ethereum 0x152442d77e9fb9c210953d583cbb2da88027fcb9 0x282fd46e108e40a45e4ce425ba75f80245e6c2e0
